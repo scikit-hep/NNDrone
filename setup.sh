652cd28fe0ddec61e2e092808bb58f2dbdea4a6b
Konstantin Gizdov <kgizdov@gmail.com>
2018-11-13 22:21:02 +0000

    better env setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-PWD=$(pwd)
-export NNDRONEHOME=${PWD} # Location of the package
+if [ -z ${BASH_SOURCE+x} ]; then
+    dir="$(cd -- "$(dirname -- "$0")" && pwd)"
+else
+    dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+fi
+export NNDRONEHOME=${dir} # Location of the package
 export PYTHONPATH=${NNDRONEHOME}:${PYTHONPATH}
