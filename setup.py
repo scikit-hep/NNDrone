5a6c2f70f74a4a84798c11204bbe96330854b2be
Konstantin Gizdov <kgizdov@gmail.com>
2018-12-13 18:37:06 +0000

    fix for Travis imports

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
-import sys
-from setuptools import setup, find_packages
-
-pkgs = None
-if sys.version_info >= (3, 3):
-    from setuptools import find_namespace_packages
-    pkgs = find_namespace_packages(include=['nndrone*', 'utilities*'])
-else:
-    pkgs = find_packages(include=['nndrone*', 'utilities*'])
+from setuptools import setup
+try:
+    from setuptools import find_namespace_packages as findpacks
+except ImportError as e:
+    from setuptools import find_packages as findpacks
 
 setup(
    name='nndrone',
    version='1.0',
    description='ML drone creation',
    author='Sean Benson, Konstantin Gizdov',
-   packages=pkgs,
+   author_email='sean.benson@cern.ch, k.gizdov@cern.ch',
+   url='https://github.com/scikit-hep/NNDrone',
+   packages=findpacks(include=['nndrone*', 'utilities*']),
    install_requires=['numpy', 'sklearn', 'scipy', 'matplotlib', 'keras']
 )
